beb4cd19398b48f882f0feb4f644ee502c8b99f9
George Jenkins <georgejenkins@Georges-iMac.local>
2016-07-10 19:55:29 -0700

    install subtheme

---
+++ sites/all/themes/fractal_farms/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function fractal_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function fractal_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function fractal_preprocess_node(&$variables) {
+}
